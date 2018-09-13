376ac6360daffd70ad03651878e002459e9e71c1
Gianatassio, Dominic Kanyo <dgianatassio@hk223-27702.local>
2018-09-13 10:11:59 -0700

    Initial Commit

---
+++ Inheritance Fun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Inheritance Fun
+//
+//  Created by Gianatassio, Dominic Kanyo on 9/13/18.
+//  Copyright © 2018 Gianatassio, Dominic Kanyo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
